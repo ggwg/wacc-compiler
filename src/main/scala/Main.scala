0c96d00e267ccf8d6cdac27f1251cb888769790c
Sudarshan Sreeram <sudarshan.sreeram19@imperial.ac.uk>
2021-01-26 20:58:29 +0000

    Add tests in MainSpec.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,4 +2,6 @@ object Main {
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
   }
+
+  def square(x: Int): Int = x * x
 }
---
+++ src/test/scala/MainSpec.scala
@@ -0,0 +1,9 @@
+import org.scalatest.flatspec.AnyFlatSpec
+
+class MainSpec extends AnyFlatSpec {
+  "Square" should "square a given number" in {
+    assert(Main.square(2) === 4)
+    assert(Main.square(4) === 16)
+    assert(Main.square(8) === 64)
+  }
+}
