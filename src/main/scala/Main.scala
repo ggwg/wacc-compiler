3ff9d017b3af9c012c5801d042c086ee1f8e1365
codrin <codrinsah@gmail.com>
2021-01-29 20:26:38 +0200

    Add parsley dependency in build.sbt.
    Contributors: [dp1119] [cc1619] [gjw19] [ss8119]

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,13 @@
+import com.wacc.functions.Program
+
+import scala.io.Source
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
-    println("Hello cruel world!")
-    print("something")
+    val filename = "/home/codrin/wacc_examples/valid/basic/skip/comment.wacc"
+    for (line <- Source.fromFile(filename).getLines()) {
+      println(line)
+    }
   }
 
   def square(x: Int): Int = x * x
