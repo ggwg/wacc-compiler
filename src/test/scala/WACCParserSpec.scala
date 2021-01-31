8a2758b950ffac36c0b1f424d0d5b69afceadb9f
codrin <codrinsah@gmail.com>
2021-02-01 00:02:39 +0200

    Create the WACCParser test suite, and initialise it with one empty test for each parser. [@dp1119] [@cc1619]

---
+++ src/test/scala/WACCParserSpec.scala
@@ -0,0 +1,65 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import WACCParser._
+
+class WACCParserSpec extends AnyFlatSpec {
+  "A program parser" should "" in {}
+
+  "A function parser" should "" in {}
+
+  "A parameter list parser" should "" in {}
+
+  "A parameter parser" should "" in {}
+
+  "A statement parser" should "" in {}
+
+  "An assign left parser" should "" in {}
+
+  "An assign right parser" should "" in {}
+
+  "An argument list parser" should "" in {}
+
+  "A pair element parser" should "" in {}
+
+  "A type parser" should "" in {}
+
+  "A base type parser" should "" in {}
+
+  "An array type parser" should "" in {}
+
+  "A pair type parser" should "" in {}
+
+  "A pair element type parser" should "" in {}
+
+  "An expression parser" should "" in {}
+
+  "An unary operator parser" should "" in {}
+
+  "A binary operator parser" should "" in {}
+
+  "An identifier parser" should "" in {}
+
+  "An array element parser" should "" in {}
+
+  "An integer literal parser" should "" in {}
+
+  "A digit parser" should "" in {}
+
+  "An integer sign parser" should "" in {}
+
+  "A boolean literal parser" should "" in {}
+
+  "A character literal parser" should "" in {}
+
+  "A string literal parser" should "" in {}
+
+  "A default character parser" should "" in {}
+
+  "An escaped character parser" should "" in {}
+
+  "An array literal parser" should "" in {}
+
+  "A pair literal parser" should "" in {}
+
+  "A comment parser" should "" in {}
+
+}
