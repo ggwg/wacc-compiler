0c31c20928bbbffa86d5c09fba51fcce2c7cf80e
Sudarshan Sreeram <sudarshan.sreeram19@imperial.ac.uk>
2021-02-12 09:28:22 +0000

    Update Type.scala with minor refactoring
    Note: FunctionType now has a toString method and some case classes were moved around to make sure that anything that's marked TODO is at the top of the file. Also, the toString method for the ArrayType was fixed.

--- src/main/scala/com/wacc/Type.scala
+++ src/main/scala/com/wacc/Type.scala
@@ -4,78 +4,79 @@ import parsley.Parsley
 import parsley.implicits.{voidImplicitly => _, _}
 
 sealed trait Type extends ASTNodeVoid {
-  def unifies(otherType: Type) = this == otherType
+  def unifies(otherType: Type): Boolean = this == otherType
   def asPairElemType(): PairElementType
 }
 
 sealed trait PairElementType extends ASTNodeVoid
-
 sealed trait BaseType extends Type with PairElementType
 
-/* New types for categorising statements in Semantic Analysis */
+/* TODO
+   - A pair is used in the nested case. It's type should be the type of another pair... */
+case class PairDefault() extends PairElementType {
+  override def toString: String = "pair"
+  // override def getType(symbolTable: SymbolTable): Type = VoidType()
+}
+
+/* ✅ Done */
+case class PairType(fstType: PairElementType, sndType: PairElementType) extends Type {
+  override def toString: String = "pair(" + fstType.toString + ", " + sndType.toString + ")"
+  override def getType(symbolTable: SymbolTable): Type = PairType(fstType, sndType)
+  override def asPairElemType(): PairElementType = PairDefault()
+}
+
+/* ✅ Done */
+case class ArrayType(arrayType: Type) extends Type with PairElementType {
+  override def toString: String = arrayType.toString + "[]"
+  override def getType(symbolTable: SymbolTable): Type = ArrayType(arrayType)
+  override def asPairElemType(): PairElementType = ArrayType(arrayType)
+}
+
+/* ✅ Done - New type for categorising statements in semantic analysis */
 case class VoidType() extends Type with PairElementType {
   override def unifies(otherType: Type): Boolean = false
-
   override def asPairElemType(): PairElementType = VoidType()
 }
 
+/* ✅ Done */
 case class FunctionType(returnType: Type, parameters: Option[List[Type]]) extends Type {
   override def asPairElemType(): PairElementType = VoidType()
+  override def toString: String = "(" +
+    (parameters match {
+      case Some(list: List[Type]) => list.map(_.toString).mkString(", ")
+      case None                   => ""
+    }) + ") => " + returnType
 }
 
-//// Change to case object - drop the brackets
-//case class IntType()(pos: (Int, Int)) extends BaseType {
-//  override def toString: String = "int"
-//  override def getType(symbolTable: SymbolTable): Type = IntType()
-//}
-// Change to case object - drop the brackets
+/* ✅ Done */
 case class IntType() extends BaseType {
   override def toString: String = "int"
   override def getType(symbolTable: SymbolTable): Type = IntType()
   override def asPairElemType(): PairElementType = IntType()
 }
 
+/* ✅ Done */
 case class BooleanType() extends BaseType {
   override def toString: String = "bool"
   override def getType(symbolTable: SymbolTable): Type = BooleanType()
   override def asPairElemType(): PairElementType = BooleanType()
 }
 
+/* ✅ Done */
 case class CharacterType() extends BaseType {
   override def toString: String = "char"
   override def getType(symbolTable: SymbolTable): Type = CharacterType()
   override def asPairElemType(): PairElementType = CharacterType()
 }
 
+/* ✅ Done */
 case class StringType() extends BaseType {
   override def toString: String = "string"
   override def getType(symbolTable: SymbolTable): Type = StringType()
   override def asPairElemType(): PairElementType = StringType()
 }
 
-case class PairDefault() extends PairElementType {
-  override def toString: String = "pair"
-  // override def getType(symbolTable: SymbolTable): Type = VoidType()
-}
-
-case class PairType(elementType1: PairElementType, elementType2: PairElementType) extends Type {
-  override def toString: String =
-    "pair(" + elementType1.toString + ", " + elementType2.toString + ")"
-  override def getType(symbolTable: SymbolTable): Type =
-    PairType(elementType1, elementType2)
-  override def asPairElemType(): PairElementType = PairDefault()
-}
-
-case class ArrayType(arrayType: Type) extends Type with PairElementType {
-  override def toString: String = "type[" + arrayType.toString + "]"
-
-  // TODO - define check
-
-  override def getType(symbolTable: SymbolTable): Type = ArrayType(arrayType)
-
-  override def asPairElemType(): PairElementType = ArrayType(arrayType)
-}
-
+/* ✅ Done */
 object BaseType {
   val types = List("int", "bool", "char", "string")
   def apply(typeString: Parsley[String]): Parsley[BaseType] = typeString.map {
@@ -86,15 +87,18 @@ object BaseType {
   }
 }
 
+/* ✅ Done */
 object PairDefault {
   def apply(string: Parsley[String]): Parsley[PairDefault] = string.map(_ => PairDefault())
 }
 
+/* ✅ Done */
 object PairType {
-  def apply(type1: Parsley[PairElementType], type2: Parsley[PairElementType]): Parsley[PairType] =
-    (type1, type2).map(PairType(_, _))
+  def apply(fstType: Parsley[PairElementType], sndType: Parsley[PairElementType]): Parsley[PairType] =
+    (fstType, sndType).map(PairType(_, _))
 }
 
+/* ✅ Done */
 object ArrayType {
   def apply(arrayType: Parsley[Type]): Parsley[ArrayType] = arrayType.map(ArrayType(_))
 }
