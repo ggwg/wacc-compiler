247526c650ac984989f57a447f1f690a8d4635e3
Sudarshan Sreeram <sudarshan.sreeram19@imperial.ac.uk>
2021-02-12 16:26:31 +0000

    Update errors in Expression.scala

--- src/main/scala/com/wacc/Error.scala
+++ src/main/scala/com/wacc/Error.scala
@@ -61,6 +61,43 @@ case object FunctionCallError {
   }
 }
 
+case object ArrayElementError {
+  def expectation(name: String, expected: String, actual: String, position: (Int, Int), code: Int = 200): Error = {
+    val message = "Invalid index in attempt to access element of array \"" + name +
+      "\":\n\t  Array index expected type " + Error.formatYellow(expected) + ", but found " + Error.formatYellow(actual)
+    Error(message, position, code)
+  }
+
+  def missing(name: String, position: (Int, Int), code: Int = 200): Error = {
+    val message = "Array index not specified in attempt to access array " + name
+    Error(message, position, code)
+  }
+}
+
+case object IdentifierError {
+  def undefined(name: String, position: (Int, Int), code: Int = 200): Error = {
+    val message = "Undefined identifier \"" + name + "\""
+    Error(message, position, code)
+  }
+}
+
+case object ArrayLiterError {
+  def expectation(expected: String, actual: String, position: (Int, Int), code: Int = 200): Error = {
+    val message = "Type mismatch in array assignment:\n\t  Array elements must be of type " +
+      Error.formatYellow(expected) + ", but found " + Error.formatYellow(actual)
+    Error(message, position, code)
+  }
+}
+
+case object PairElementError {
+  def expectation(expected: String, actual: String, isFirst: Boolean, position: (Int, Int), code: Int = 200): Error = {
+    val function = if (isFirst) "fst" else "snd"
+    val message = "Pre-defined function " + function + " expected " + Error.formatYellow(expected) + ", but found " +
+      Error.formatYellow(actual)
+    Error(message, position, code)
+  }
+}
+
 object Error {
   def formatYellow(message: String): String = Console.YELLOW + message + Console.RESET
   def formatRed(message: String): String = Console.RED + message + Console.RESET
--- src/main/scala/com/wacc/Expression.scala
+++ src/main/scala/com/wacc/Expression.scala
@@ -2,7 +2,7 @@ package com.wacc
 
 import com.wacc.operator._
 import parsley.Parsley
-import parsley.Parsley.pos
+import parsley.Parsley.{get, pos}
 import parsley.implicits.{voidImplicitly => _, _}
 
 import scala.collection.mutable
@@ -20,7 +20,6 @@ case class UnaryOperatorApplication(operator: UnaryOperator, operand: Expression
   override def check(symbolTable: SymbolTable)(implicit errors: mutable.ListBuffer[Error]): Unit = {
 
     /* Get the operand type */
-
     val operandType = operand.getType(symbolTable)
 
     /* Error generation process */
@@ -29,28 +28,28 @@ case class UnaryOperatorApplication(operator: UnaryOperator, operand: Expression
       /* Chr accepts an integer parameter */
       case Chr() =>
         if (!operandType.unifies(IntType())) {
-          errors += UnaryOperatorError("chr", "int", operandType.toString, operand.getPos())
+          errors += UnaryOperatorError.expectation("chr", "int", operandType.toString, operand.getPos())
           return
         }
 
       /* Negate accepts an integer parameter */
       case Negate() =>
         if (!operandType.unifies(IntType())) {
-          errors += UnaryOperatorError("(-) (i.e. negate)", "int", operandType.toString, operand.getPos())
+          errors += UnaryOperatorError.expectation("(-) (i.e. negate)", "int", operandType.toString, operand.getPos())
           return
         }
 
       /* Not accepts a boolean parameter */
       case Not() =>
         if (!operandType.unifies(BooleanType())) {
-          errors += UnaryOperatorError("not", "boolean", operandType.toString, operand.getPos())
+          errors += UnaryOperatorError.expectation("not", "boolean", operandType.toString, operand.getPos())
           return
         }
 
       /* Ord accepts a character parameter */
       case Ord() =>
         if (!operandType.unifies(CharacterType())) {
-          errors += UnaryOperatorError("ord", "char", operandType.toString, operand.getPos())
+          errors += UnaryOperatorError.expectation("ord", "char", operandType.toString, operand.getPos())
           return
         }
 
@@ -59,7 +58,7 @@ case class UnaryOperatorApplication(operator: UnaryOperator, operand: Expression
         operandType match {
           case ArrayType(_) | EmptyType() => ()
           case _ =>
-            errors += UnaryOperatorError("length", "array", operandType.toString, operand.getPos())
+            errors += UnaryOperatorError.expectation("length", "array", operandType.toString, operand.getPos())
             return
         }
     }
@@ -94,13 +93,12 @@ case class FunctionCall(name: Identifier, arguments: Option[ArgumentList])(posit
 
     if (func.isEmpty) {
       /* Invalid call to a function that's undefined */
-      errors += DefaultError("Function " + name.identifier + " not defined in scope", name.getPos())
+      errors += FunctionCallError.undefined(name.identifier, name.getPos())
       return
     }
 
     func.get._2 match {
-
-      case Function(returnType: Type, _: Identifier, params: Option[ParameterList], _: Statement) =>
+      case Function(returnType: Type, name: Identifier, params: Option[ParameterList], _: Statement) =>
         /* Extract the expected parameter types from the function signature */
         val expectedParams = {
           params match {
@@ -122,7 +120,12 @@ case class FunctionCall(name: Identifier, arguments: Option[ArgumentList])(posit
         /* Check that the supplied parameters match the signature */
         if (!expectedSignature.unifies(calledSignature)) {
           /* The signatures of the functions don't match - type mismatch in the caller arguments or return type */
-          errors += DefaultError("Type mismatch for Functions with function in symbol table (TODO)", getPos())
+          errors += FunctionCallError.expectation(
+            name.identifier,
+            expectedSignature.toString,
+            calledSignature.toString,
+            getPos()
+          )
           return
         }
 
@@ -130,11 +133,7 @@ case class FunctionCall(name: Identifier, arguments: Option[ArgumentList])(posit
         arguments.foreach(_.check(symbolTable))
       case _ =>
         /* Invalid function call with an identifier that's not a function */
-        errors +=
-          DefaultError(
-            "Function call " + name.identifier + " is not of type Function. Got of type " + func.get._1,
-            getPos()
-          )
+        errors += FunctionCallError.invalid(name.identifier, func.get._1.toString, getPos())
     }
   }
 
@@ -163,14 +162,16 @@ case class ArrayElement(name: Identifier, expressions: List[Expression])(positio
 
   override def check(symbolTable: SymbolTable)(implicit errors: mutable.ListBuffer[Error]): Unit = {
     if (expressions.isEmpty) {
-      errors += DefaultError("No array index specified in attempt to access array " + name.identifier, getPos())
+      errors += ArrayElementError.missing(name.identifier, getPos())
     } else {
       /* Go through each expression and check if it's of type int */
       expressions.foreach(expression => {
         val expressionType = expression.getType(symbolTable)
         if (!expressionType.unifies(IntType()))
-          errors += DefaultError(
-            "Array index expected type int, but found " + expressionType.getType(symbolTable).toString,
+          errors += ArrayElementError.expectation(
+            name.identifier,
+            IntType.toString(),
+            expressionType.toString,
             getPos()
           )
       })
@@ -214,10 +215,10 @@ case class BinaryOperatorApplication(leftOperand: Expression, operator: BinaryOp
       /* Integer operations expect integer parameters */
       case Add() | Divide() | Modulo() | Multiply() | Subtract() =>
         if (!leftType.unifies(IntType())) {
-          errors += BinaryOperatorError(op, IntType.toString(), leftType.toString, position, isLeft = true)
+          errors += BinaryOperatorError.expectation(op, IntType.toString(), leftType.toString, getPos(), "left")
           return
         } else if (!rightType.unifies(IntType())) {
-          errors += BinaryOperatorError(op, IntType.toString(), rightType.toString, position, isLeft = false)
+          errors += BinaryOperatorError.expectation(op, IntType.toString(), rightType.toString, getPos(), "right")
           return
         }
 
@@ -226,31 +227,27 @@ case class BinaryOperatorApplication(leftOperand: Expression, operator: BinaryOp
         val expected = IntType.toString() + " or " + CharacterType.toString()
 
         if (!(leftType.unifies(IntType()) || leftType.unifies(CharacterType()))) {
-          errors += BinaryOperatorError(op, expected, leftType.toString, position, isLeft = true)
+          errors += BinaryOperatorError.expectation(op, expected, leftType.toString, getPos(), "left")
           return
         } else if (!(rightType.unifies(IntType()) || rightType.unifies(CharacterType()))) {
-          errors += BinaryOperatorError(op, expected, rightType.toString, position, isLeft = false)
+          errors += BinaryOperatorError.expectation(op, expected, rightType.toString, getPos(), "right")
           return
         }
 
       /* Equal operations expect any same types */
       case Equals() | NotEquals() =>
         if (!leftType.unifies(rightType)) {
-          errors += DefaultError(
-            "Cannot compare " + leftType.toString + " and " + rightType.toString + " types " +
-              " in " + this.toString,
-            position
-          )
+          errors += BinaryOperatorError.comparison(op, leftType.toString, rightType.toString, getPos())
           return
         }
 
       /* Boolean operations expect boolean types */
       case And() | Or() =>
         if (!leftType.unifies(BooleanType())) {
-          errors += BinaryOperatorError(op, BooleanType.toString(), leftType.toString, position, isLeft = true)
+          errors += BinaryOperatorError.expectation(op, BooleanType.toString(), leftType.toString, getPos(), "left")
           return
         } else if (!rightType.unifies(BooleanType())) {
-          errors += BinaryOperatorError(op, BooleanType.toString(), rightType.toString, position, isLeft = false)
+          errors += BinaryOperatorError.expectation(op, BooleanType.toString(), rightType.toString, getPos(), "right")
           return
         }
     }
@@ -291,7 +288,7 @@ case class Identifier(identifier: String)(position: (Int, Int)) extends Expressi
 
     if (getType(symbolTable).unifies(VoidType())) {
       /* Identifier was a void type - indicating that it hasn't been defined yet */
-      errors += DefaultError("Undefined identifier \"" + identifier + "\"", position)
+      errors += IdentifierError.undefined(identifier, getPos())
     }
   }
 
@@ -312,7 +309,6 @@ case class IntegerLiter(sign: Option[IntegerSign], digits: List[Digit])(position
   override def getPos(): (Int, Int) = position
   override def getType(symbolTable: SymbolTable): Type = IntType()
   override def check(symbolTable: SymbolTable)(implicit errors: ListBuffer[Error]): Unit = {
-
     /* Map the characters to digits */
     val intDigits = digits.map(_.digit - '0')
     var value: Long = 0
@@ -325,9 +321,17 @@ case class IntegerLiter(sign: Option[IntegerSign], digits: List[Digit])(position
     for (i <- intDigits) {
       value = (value * 10) + i
       if (value > Integer.MAX_VALUE && signValue == 1)
-        errors += DefaultError("Max bound of int exceeded...", getPos())
+        errors += Error(
+          "Maximum value bound of type " + Error.formatYellow(IntType.toString()) + " exceeded!",
+          getPos(),
+          100
+        )
       else if (-value < Integer.MIN_VALUE && signValue == -1)
-        errors += DefaultError("Min bound of int exceeded...", getPos())
+        errors += Error(
+          "Minimum value bound of type " + Error.formatYellow(IntType.toString()) + " exceeded!",
+          getPos(),
+          100
+        )
     }
   }
 }
@@ -353,13 +357,9 @@ case class ArrayLiter(expressions: List[Expression])(position: (Int, Int)) exten
 
       /* All other elements must have the same type */
       for (expression <- expressions.tail) {
-        if (!expression.getType(symbolTable).unifies(arrayElementType)) {
-          errors += DefaultError(
-            "Array assignment type mismatch: Got type " + expression.getType(
-              symbolTable
-            ) + ", Expected " + arrayElementType,
-            expression.getPos()
-          )
+        val expressionType = expression.getType(symbolTable)
+        if (!expressionType.unifies(arrayElementType)) {
+          errors += ArrayLiterError.expectation(arrayElementType.toString, expressionType.toString, expression.getPos())
           return
         }
       }
@@ -437,9 +437,11 @@ case class PairElement(expression: Expression, isFirst: Boolean)(position: (Int,
   override def check(symbolTable: SymbolTable)(implicit errors: mutable.ListBuffer[Error]): Unit = {
 
     /* The expression must be a pair */
-    expression.getType(symbolTable) match {
+    val expressionType = expression.getType(symbolTable)
+    expressionType match {
       case PairType(_, _) => ()
-      case _              => errors += DefaultError("expression is not a pair", expression.getPos())
+      case _ =>
+        errors += PairElementError.expectation("pair", expressionType.toString, isFirst, expression.getPos())
     }
 
     expression.check(symbolTable)
@@ -463,13 +465,11 @@ case class PairElement(expression: Expression, isFirst: Boolean)(position: (Int,
 
 /*  -----------------------------  Class objects  -----------------------------  */
 
-/* ✅ Done */
 object ArrayElement {
   def apply(identifier: Parsley[Identifier], expressions: Parsley[List[Expression]]): Parsley[ArrayElement] =
     pos <**> (identifier, expressions).map(ArrayElement(_, _))
 }
 
-/* ✅ Done */
 object ArrayLiter {
   def apply(maybeExpressions: Parsley[Option[(Expression, List[Expression])]]): Parsley[ArrayLiter] =
     pos <**> maybeExpressions.map {
@@ -478,7 +478,6 @@ object ArrayLiter {
     }
 }
 
-/* ✅ Done */
 object BinaryOperatorApplication {
   def apply(
     leftOperand: Parsley[Expression],
@@ -488,65 +487,54 @@ object BinaryOperatorApplication {
     pos <**> (leftOperand, operator, rightOperand).map(BinaryOperatorApplication(_, _, _))
 }
 
-/* ✅ Done */
 object BooleanLiter {
   def apply(bool: Parsley[String]): Parsley[BooleanLiter] = pos <**> bool.map(bool => BooleanLiter(bool.equals("true")))
 }
 
-/* ✅ Done */
 object CharacterLiter {
   def apply(character: Parsley[DefaultCharacter]): Parsley[CharacterLiter] =
     pos <**> character.map(character => CharacterLiter(character.char))
 }
 
-/* ✅ Done */
 object Identifier {
   def apply(prefix: Parsley[String], suffix: Parsley[List[Char]]): Parsley[Identifier] =
     pos <**> (prefix, suffix).map((prefix, suffix) => Identifier(prefix + suffix.mkString))
 }
 
-/* ✅ Done */
 object IntegerLiter {
   def apply(sign: Parsley[Option[IntegerSign]], digits: Parsley[List[Digit]]): Parsley[IntegerLiter] =
     pos <**> (sign, digits).map(IntegerLiter(_, _))
 }
 
-/* ✅ Done */
 object PairLiter {
   def apply(string: Parsley[String]): Parsley[PairLiter] = pos <**> string.map(_ => PairLiter())
 }
 
-/* ✅ Done */
 object StringLiter {
   def apply(characters: Parsley[List[DefaultCharacter]]): Parsley[StringLiter] =
     pos <**> characters.map(character => StringLiter(character.mkString))
 }
 
-/* ✅ Done */
 object UnaryOperatorApplication {
   def apply(operator: Parsley[UnaryOperator], operand: Parsley[Expression]): Parsley[UnaryOperatorApplication] =
     pos <**> (operator, operand).map(UnaryOperatorApplication(_, _))
 }
 
-/* ✅ Done */
 object FunctionCall {
   def apply(name: Parsley[Identifier], arguments: Parsley[Option[ArgumentList]]): Parsley[FunctionCall] =
     pos <**> (name, arguments).map(FunctionCall(_, _))
 }
 
-/* ✅ Done */
 object NewPair {
   def apply(first: Parsley[Expression], second: Parsley[Expression]): Parsley[NewPair] =
     pos <**> (first, second).map(NewPair(_, _))
 }
 
-/* ✅ Done */
 object PairElement {
   def apply(expression: Parsley[Expression], isFirst: Boolean): Parsley[PairElement] =
     pos <**> expression.map(PairElement(_, isFirst))
 }
 
-/* ✅ Done */
 object ArgumentList {
   def apply(expression: Parsley[Expression], expressions: Parsley[List[Expression]]): Parsley[ArgumentList] =
     (expression, expressions).map((e, es) => ArgumentList(e :: es))
