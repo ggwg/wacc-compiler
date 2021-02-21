d208c626d0f11bbd34b73cd36d3e912a0dbc5d53
codrin <codrinsah@gmail.com>
2021-02-21 14:53:27 +0200

    Add compile function to ASTNode trait[@dp1119] [@cc1619] [@gjw19] [@ss8119]

--- src/main/scala/com/wacc/ASTNodeVoid.scala
+++ src/main/scala/com/wacc/ASTNodeVoid.scala
@@ -19,6 +19,11 @@ trait ASTNode {
 
   /* Returns the actual type of a node */
   def getType(symbolTable: SymbolTable): Type
+
+  def compile(declaredVars: List[String], declaredSize: Int)(implicit
+    instructions: mutable.ListBuffer[Instruction],
+    varDic: mutable.Map[String, mutable.Stack[Int]]
+  ): Unit = ()
 }
 
 /* Create a new trait that extends ASTNode and defined default void return */
