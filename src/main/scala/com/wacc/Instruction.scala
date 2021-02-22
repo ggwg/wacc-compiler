ddef9081bfecf8826189bef64bef0357fd46187a
codrin <codrinsah@gmail.com>
2021-02-22 16:48:46 +0200

    Added toString methods to all Instruction classes [@dp1119] [@cc1619]

--- src/main/scala/com/wacc/Instruction.scala
+++ src/main/scala/com/wacc/Instruction.scala
@@ -6,37 +6,125 @@ sealed trait AddressAccess
 sealed trait Condition
 sealed trait Register extends Operand2
 
-case object Register0 extends Register
-case object Register1 extends Register
-case object Register2 extends Register
-case object Register3 extends Register
-case object Register4 extends Register
-case object Register5 extends Register
-case object Register6 extends Register
-case object Register7 extends Register
-case object Register8 extends Register
-case object Register10 extends Register
-case object Register11 extends Register
-case class ImmediateValue(value: Int) extends Operand2
+case object Register0 extends Register {
+  override def toString: String = "r0"
+}
 
-case class ImmediateLoad(value: Int) extends AddressAccess
-case class RegisterLoad(id: Int) extends AddressAccess
-case class RegisterOffsetLoad(id: Int, offset: ImmediateValue) extends AddressAccess
+case object Register1 extends Register {
+  override def toString: String = "r1"
+}
 
-case object Equal extends Condition
-case object NotEqual extends Condition
-case object LessOrEqual extends Condition
-case object GreaterOrEqual extends Condition
-case object Less extends Condition
-case object Greater extends Condition
+case object Register2 extends Register {
+  override def toString: String = "r2"
+}
 
-case class PushLR() extends Instruction
-case class PopPC() extends Instruction
-case class Add(dest: Register, src: Register, op2: Operand2) extends Instruction
-case class Sub(dest: Register, src: Register, op2: Operand2) extends Instruction
-case class Mul(dest: Register, multiplier: Register, src: Register) extends Instruction
-case class Load(dest: Register, src: AddressAccess) extends Instruction
-case class Store(src: Register, dest: AddressAccess) extends Instruction
-case class Branch(cond: Option[Condition], label: String) extends Instruction
-case class Compare(src: Register, op2: Operand2) extends Instruction
-case class Move(dest: Register, op2: Operand2) extends Instruction
+case object Register3 extends Register {
+  override def toString: String = "r3"
+}
+
+case object Register4 extends Register {
+  override def toString: String = "r4"
+}
+
+case object Register5 extends Register {
+  override def toString: String = "r5"
+}
+
+case object Register6 extends Register {
+  override def toString: String = "r6"
+}
+
+case object Register7 extends Register {
+  override def toString: String = "r7"
+}
+case object Register8 extends Register {
+  override def toString: String = "r8"
+}
+
+case object Register10 extends Register {
+  override def toString: String = "r10"
+}
+
+case object Register11 extends Register {
+  override def toString: String = "r10"
+}
+
+case class ImmediateValue(value: Int) extends Operand2 {
+  override def toString: String = "#" + value
+}
+
+case class ImmediateLoad(value: Int) extends AddressAccess {
+  override def toString: String = "=" + value
+}
+
+case class RegisterLoad(reg: Register) extends AddressAccess {
+  override def toString: String = s"[$reg]"
+}
+
+case class RegisterOffsetLoad(reg: Register, offset: ImmediateValue) extends AddressAccess {
+  override def toString: String = s"[$reg, $offset]"
+}
+
+case object Equal extends Condition {
+  override def toString: String = "EQ"
+}
+
+case object NotEqual extends Condition {
+  override def toString: String = "NE"
+}
+
+case object LessOrEqual extends Condition {
+  override def toString: String = "LE"
+}
+
+case object GreaterOrEqual extends Condition {
+  override def toString: String = "GE"
+}
+
+case object Less extends Condition {
+  override def toString: String = "LT"
+}
+
+case object Greater extends Condition {
+  override def toString: String = "GT"
+}
+
+case class PushLR() extends Instruction {
+  override def toString: String = "PUSH {LR}"
+}
+
+case class PopPC() extends Instruction {
+  override def toString: String = "POP {PC}"
+}
+
+case class Add(dest: Register, src: Register, op2: Operand2) extends Instruction {
+  override def toString: String = s"ADD $dest $src $op2"
+}
+
+case class Sub(dest: Register, src: Register, op2: Operand2) extends Instruction {
+  override def toString: String = s"SUB $dest $src $op2"
+}
+
+case class Mul(dest: Register, multiplier: Register, src: Register) extends Instruction {
+  override def toString: String = s"MUL $dest $multiplier $src"
+}
+
+case class Load(dest: Register, src: AddressAccess) extends Instruction {
+  override def toString: String = s"LDR $dest $src"
+}
+
+case class Store(src: Register, dest: AddressAccess) extends Instruction {
+  override def toString: String = s"STR $src $dest"
+}
+
+case class Branch(cond: Option[Condition], label: String) extends Instruction {
+  override def toString: String = s"B${cond.map(_.toString).getOrElse("")} $label"
+}
+
+case class Compare(src: Register, op2: Operand2) extends Instruction {
+  override def toString: String = s"CMP $src $op2"
+}
+
+case class Move(dest: Register, op2: Operand2) extends Instruction {
+  override def toString: String = s"MOV $dest $op2"
+}
