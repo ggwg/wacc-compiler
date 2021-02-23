1fbe029d72b22c61f332da3f89a50615f899e5f2
codrin <codrinsah@gmail.com>
2021-02-23 12:02:04 +0200

    Added a label ID generator in AssemblerState [@dp1119] [@cc1619]

--- src/main/scala/com/wacc/AssemblerState.scala
+++ src/main/scala/com/wacc/AssemblerState.scala
@@ -5,6 +5,10 @@ import scala.collection.immutable
 case class AssemblerState(
   val varDic: immutable.Map[String, Int],
   val freeRegs: immutable.List[Register],
+  val nextLabelID: Iterator[Int] = LazyList.iterate(0)(_ + 1).iterator,
   val declaredVars: immutable.List[String],
   val declaredSize: Int
-)
+) {
+
+  def nextID: Int = nextLabelID.next
+}
