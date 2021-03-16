afc30081e9303441b68664905b73b106073bfd24
codrin <codrinsah@gmail.com>
2021-03-16 21:59:17 +0200

    Add an extension test file [@dp1119] [@cc1619]

--- src/test/scala/ExtensionSpec.scala
+++ src/test/scala/ExtensionSpec.scala
@@ -1,3 +1,16 @@
-class ExtensionSpec {
+import Parser.statementParser
+import com.wacc.For
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.{a, convertToAnyShouldWrapper}
+
+class ExtensionSpec extends AnyFlatSpec {
+  "The compiler" should "successfully compile a program using a for statement" in {
+    statementParser
+      .runParser("for (int i = 0, j = 0; i < n && j < n; i = i + 1, j = j + 1) do skip done")
+      .get shouldBe a[For]
+    statementParser
+      .runParser("for (;;) do skip done")
+      .get shouldBe a[For]
+  }
 
 }
